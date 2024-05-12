ef3ffd96423b6a5eafe8cfb2c9192c0b8c56c7f8
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-12 18:46:36 +0900

    Completes 02_repeatString with string.repeat(num)

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(s, n) {
+    if (s == "" || n == 0) {
+        return "";
+    } else if (n < 0) {
+        return "ERROR";
+    } else {
+        return s.repeat(n);
+    } 
 };
 
 // Do not edit below this line
