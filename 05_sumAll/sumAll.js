1fde4ed3961b3ef23c39f36f16a89bd3af7da792
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-12 21:46:36 +0900

    Completes 05_sumAll with while loop after check

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0) {
+        return "ERROR";
+    } else {
+        let temp;
+        let sum = 0;
 
+        if (a > b) {
+            temp = b;
+            b = a;
+            a = temp;
+        }
+
+        while (b >= a) {
+            sum += b;
+            b--;
+        }
+
+        return sum;
+    }
 };
 
 // Do not edit below this line
