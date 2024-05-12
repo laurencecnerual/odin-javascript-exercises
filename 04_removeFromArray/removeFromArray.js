ceba5e18d2da585a34303d64ca54dfc5ee2c939b
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-12 21:34:24 +0900

    Completes 04_removeFromArray with nested for loops

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(oldList, ...removeMe) {
+    const newList = [];
 
+    for (e of oldList) {
+        let isFound = false;
+
+        for (r of removeMe) {
+            if (e === r) {
+                isFound = true;
+            }
+        }
+
+        if (!isFound) {
+            newList.push(e);
+        }
+    }
+
+    return newList;
 };
 
 // Do not edit below this line
