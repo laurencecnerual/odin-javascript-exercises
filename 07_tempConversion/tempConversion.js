ee4a01330f5a7cfa2c052f2d65d78618dc9d2509
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-12 22:03:34 +0900

    Completes 07_tempConversion - computes and rounds

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let c = 5 / 9 * (f - 32)
+  
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let f = (c * 9) / 5 + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
