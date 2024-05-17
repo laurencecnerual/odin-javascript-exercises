432d4a8324b926ff679a7d5e0f97a081a6dca46a
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-18 00:16:02 +0900

    Completes 09_palindromes with split, reverse, join

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letters = str.toLowerCase().split("").filter(getArrayOfLetters);
+    let reversedLetters = str.toLowerCase().split("").filter(getArrayOfLetters).reverse().join("")
+    letters = letters.join("");
+    return letters === reversedLetters;
 };
+
+function getArrayOfLetters(letter) {
+    return (/[a-z0-9]/).test(letter)
+}
 
 // Do not edit below this line
 module.exports = palindromes;
