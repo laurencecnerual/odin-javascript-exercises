2edfbd4e6509e92c47563934306bc2fe3b805dea
laurencecnerual <47619680+laurencecnerual@users.noreply.github.com>
2024-05-18 14:23:13 +0900

    Completes 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    for (const element of arr) {
+        if (Number.isInteger(element.yearOfDeath)) {
+            element.age = element.yearOfDeath - element.yearOfBirth;
+        } else {
+            element.age = new Date().getFullYear() - element.yearOfBirth;
+        }
+    }
 
+    arr.sort((a, b) => b.age - a.age);
+    return arr[0];
 };
 
 // Do not edit below this line
